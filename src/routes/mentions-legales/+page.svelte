d5dc351007d406d69856298522c8f7d4d1a0b525
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-02-25 23:00:08 +0100

    feat: add legal mentions page with contact and hosting information

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Footer from '$lib/component/Footer.svelte';
+</script>
+
+<div class="min-h-screen p-16">
+	<h1 class="text-5xl">Mentions légales</h1>
+	<p>
+		Le site amevs.fr est la propriété de l'Association Musicale et Éducative à Vocation Sociale loi
+		1901 à but non lucratif reconnue publique au JO du XX/XX/XX, dont le siège social est situé au 4
+		Place Jussieu 75005 Paris.<br />
+		SIRET XXXXXXX <br />
+		Tél. : +33 (0)7 55 90 57 02 <br />
+		Courriel : amevs.su@gmail.com
+	</p>
+	<h2 class="text-2xl">Réalisation et maintenance technique</h2>
+	<p>
+		Le site amevs.fr est réalisé par <a href="https://charles.vin/" class="link">Charles Vin</a>.
+	</p>
+	<h2 class="text-2xl">Hébergement</h2>
+	<p>
+		Le site amevs.fr est hébergé par <a href="cloudflare.com" class="link"></a>Cloudflare, Inc. 101
+		Townsend St, San Francisco, CA 94107 USA
+	</p>
+</div>
+<Footer />
