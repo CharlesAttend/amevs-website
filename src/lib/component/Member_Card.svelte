723956f0ab9fc330d77f5b098190bed3673445f0
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-02-25 19:06:11 +0100

    feat: refactor Member_Card layout for improved structure and readability

--- src/lib/component/Member_Card.svelte
+++ src/lib/component/Member_Card.svelte
@@ -3,22 +3,22 @@
 </script>
 
 <div class="card card-side bg-base-100 shadow-sm">
-  <figure class="w-1/2 px-2">
-    <div class="avatar flex items-center">
-      <div class="ring-primary ring-offset-base-100 w-full rounded-full ring ring-offset-2">
-        <img src={pp} alt={name} />
+  <div class="card-body flex-row gap-5 items-center w-full">
+    <figure class="w-1/2">
+      <div class="avatar flex items-center">
+        <div class="ring-primary ring-offset-base-100 w-full rounded-full ring ring-offset-2">
+          <img src={pp} alt={name} />
+        </div>
       </div>
+    </figure>
+    <div class="w-1/2">
+      <h2 class="card-title text-2xl">{name}</h2>
+      <p class="my-2 grid grid-cols-3 grid-rows-2 content-stretch items-stretch gap-1">
+        <span class="text-gray-500">Role : </span>
+        <span class="col-span-2">{role}</span>
+        <span class="text-gray-500">Status : </span>
+        <span class="col-span-2">{status}</span>
+      </p>
     </div>
-  </figure>
-  <div class="card-body w-full">
-    <h2 class="card-title text-2xl">{name}</h2>
-    <p class="grid grid-cols-2 content-stretch items-stretch gap-1">
-      <span class="text-gray-500">Role : </span>
-      <span>{role}</span>
-    </p>
-    <p class="grid grid-cols-2 content-stretch items-stretch gap-1">
-      <span class="text-gray-500">Status : </span>
-      <span>{status}</span>
-    </p>
   </div>
 </div>
