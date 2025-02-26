c4bfddd5e51a2d497cc961791b3ab188e3f2d3de
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-02-26 18:34:10 +0100

    feat: add HeaderSubPage component and logo, update mentions légales page layout

---
+++ src/lib/component/HeaderSubPage.svelte
@@ -0,0 +1,26 @@
+<script>
+  import LogoAmevs from "$lib/assets/logos/logo_amevs.svelte";
+
+  let breadCrumb = [
+    { title: 'Accueil', link: '/' },
+    { title: 'Mentions légales', link: '/mentions-legales' }
+  ];
+</script>
+
+<header class="w-full bg-white">
+  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <div class="flex">
+      <a href="/" class="p-1.5">
+        <span class="sr-only">AMEVS</span>
+        <LogoAmevs classAttributes="fill-current w-12"/>
+      </a>
+    </div>
+    <div class="breadcrumbs text-sm s">
+      <ul>
+        {#each breadCrumb as item}
+          <li><a href={item.link}>{item.title}</a></li>
+        {/each}
+      </ul>
+    </div>
+  </nav>
+</header>
--- src/routes/mentions-legales/+page.svelte
+++ src/routes/mentions-legales/+page.svelte
@@ -1,25 +1,33 @@
 <script>
-	import Footer from '$lib/component/Footer.svelte';
+  import Footer from '$lib/component/Footer.svelte';
+  import HeaderSubPage from '$lib/component/HeaderSubPage.svelte';
 </script>
 
-<div class="min-h-screen p-16">
+<HeaderSubPage />
+<div class="flex min-h-dvh flex-col gap-4 p-16">
 	<h1 class="text-5xl">Mentions légales</h1>
-	<p>
-		Le site amevs.fr est la propriété de l'Association Musicale et Éducative à Vocation Sociale loi
-		1901 à but non lucratif reconnue publique au JO du XX/XX/XX, dont le siège social est situé au 4
-		Place Jussieu 75005 Paris.<br />
-		SIRET XXXXXXX <br />
-		Tél. : +33 (0)7 55 90 57 02 <br />
-		Courriel : amevs.su@gmail.com
-	</p>
-	<h2 class="text-2xl">Réalisation et maintenance technique</h2>
-	<p>
-		Le site amevs.fr est réalisé par <a href="https://charles.vin/" class="link">Charles Vin</a>.
-	</p>
-	<h2 class="text-2xl">Hébergement</h2>
-	<p>
-		Le site amevs.fr est hébergé par <a href="cloudflare.com" class="link">Cloudflare</a>, Inc. 101
-		Townsend St, San Francisco, CA 94107 USA
-	</p>
+  <div>
+    <p>
+      Le site amevs.fr est la propriété de l'Association Musicale et Éducative à Vocation Sociale
+      loi 1901 à but non lucratif reconnue publique au JO du XX/XX/XX, dont le siège social est
+      situé au 4 Place Jussieu 75005 Paris.<br />
+      SIRET XXXXXXX <br />
+      Tél. : +33 (0)7 55 90 57 02 <br />
+      Courriel : amevs.su@gmail.com
+    </p>
+  </div>
+  <div>
+    <h2 class="text-2xl">Réalisation et maintenance technique</h2>
+    <p>
+      Le site amevs.fr est réalisé par <a href="https://charles.vin/" class="link">Charles Vin</a>.
+    </p>
+  </div>
+  <div>
+    <h2 class="text-2xl">Hébergement</h2>
+    <p>
+      Le site amevs.fr est hébergé par <a href="cloudflare.com" class="link">Cloudflare</a>, Inc.
+      101 Townsend St, San Francisco, CA 94107 USA
+    </p>
+  </div>
 </div>
 <Footer />
