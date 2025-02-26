5347d46a2621eb77b896dac2808ab51a090389d3
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-02-26 16:34:53 +0100

    is it the id ?

--- src/lib/section/Socials.svelte
+++ src/lib/section/Socials.svelte
@@ -12,24 +12,7 @@
   console.log('test');
 </script>
 
-<div class="hero min-h-dvh bg-secondary text-secondary-content lg:min-h-auto lg:p-16">
-  <div class="hero-content flex-col lg:flex-row lg:max-w-auto lg:item-stretch">
-    <div class="flex flex-col items-start justify-start gap-6 lg:p-20 lg:w-1/2">
-      <h1 class="text-5xl font-bold">Notre vision</h1>
-      <p class="">
-        Être un acteur clé du rayonnement mondial de la culture, offrant aux jeunes une vie
-        épanouissante, en particulier à ceux des milieux marginalisés, et favorisant l'émergence
-        d'un réseau de citoyens responsables et engagés.
-      </p>
-      <ol class="flex list-disc flex-row gap-8">
-        <li class="list-none">Pick it</li>
-        <li class="">Book it</li>
-        <li class="">Groom it</li>
-      </ol>
-      <button class="btn btn-primary w-auto self-auto">En savoir plus</button>
-    </div>
-  </div>
-</div>
+<div class="hero bg-secondary text-secondary-content lg:min-h-auto lg:p-16">HELLOOOOO</div>
 <div id="socials" class="hero bg-secondary text-secondary-content lg:min-h-auto lg:p-16">
   <div class="hero-content p-6 text-center">
     <div class="max-w-md">
