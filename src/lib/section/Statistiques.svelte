e49c444d5ecac12b78619e615e1b27594bf3eab9
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-02-26 17:48:48 +0100

    fix: update styling fo Stat section for improved visual consistency

--- src/lib/section/Statistiques.svelte
+++ src/lib/section/Statistiques.svelte
@@ -14,7 +14,7 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              d="M13 10V3L4 14h7v7l9-11h-7z"
             ></path>
           </svg>
         </div>
@@ -34,7 +34,7 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M13 10V3L4 14h7v7l9-11h-7z"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
             ></path>
           </svg>
         </div>
@@ -44,7 +44,7 @@
       </div>
 
       <div class="stat">
-        <div class="stat-figure text-secondary">
+        <div class="stat-figure text-accent">
           <div class="avatar online">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -53,15 +53,14 @@
               class="inline-block h-8 w-8 stroke-current"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13 10V3L4 14h7v7l9-11h-7z"
-              ></path>
+                style="fill:currentColor"
+                class="ColorScheme-Text"
+                d="M14,1.38317871 L14,11.5 C14,12.8807119 12.8807119,14 11.5,14 C10.1192881,14 9,12.8807119 9,11.5 C9,10.1192881 10.1192881,9 11.5,9 C11.6745932,9 11.8450063,9.01789743 12.0095047,9.05195772 L12.0279688,4.0010024 L6,5.0010024 L6,12.5 C6,13.8807119 4.88071187,15 3.5,15 C2.11928813,15 1,13.8807119 1,12.5 C1,11.1192881 2.11928813,10 3.5,10 C3.67138643,10 3.83874484,10.017246 4.00043449,10.0500973 L4,3 L14,1.38317871 Z"
+              />
             </svg>
           </div>
         </div>
-        <div class="stat-value">30</div>
+        <div class="stat-value text-accent">30</div>
         <div class="stat-title">Concert organisé</div>
         <div class="stat-desc text-secondary"></div>
       </div>
